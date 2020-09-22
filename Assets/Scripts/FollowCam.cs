74a80b714f86464be3b84c6b8564d7643b132c4e
Ryan Canuel <ryancanuel@hotmail.com>
2020-09-22 11:50:27 -0400

    FollowCam added

---
+++ Assets/Scripts/FollowCam.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    static public FollowCam S;
+
+    // fields set in the Unity Inspector pane
+    public float easing = 0.05f;
+    public Vector2 minXY;
+    public bool _____________________________;
+
+    // fields set dynamically
+    public GameObject poi;
+    public float camZ;
+
+    void Awake()
+    {
+        S = this;
+        camZ = this.transform.position.z;
+    }
+
+    void FixedUpdate()
+    {
+        if (poi == null) return;
+
+
+        // Get the position of the poi
+        Vector3 destination = poi.transform.position;
+        // Limit the X & Y to minimum values
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
+        // Interpolate from the current Camera position toward destination
+        destination = Vector3.Lerp(transform.position, destination, easing);
+        // Retain a destination.z of camZ
+        destination.z = camZ;
+        // Set the camera to the destination
+        transform.position = destination;
+        // Set the orthographicSize of the Camera to keep Ground in view
+        this.GetComponent<Camera>().orthographicSize = destination.y + 10;
+    }
+}
